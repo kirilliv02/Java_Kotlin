39111e5796d59ab1371606a036dd965e57397921
Кирилл <74520222+kirilliv02@users.noreply.github.com>
2021-09-28 16:46:30 +0300

    Update Calculator.kt

--- hw2/src/main/kotlin/Calculator.kt
+++ hw2/src/main/kotlin/Calculator.kt
@@ -1,5 +1,6 @@
 class Calculator{
     fun calculate(val1: Int, operator: String, val2: Int): Int {
+        
         return when (operator) {
             "+" -> val1 + val2
             "-" -> val1 - val2
