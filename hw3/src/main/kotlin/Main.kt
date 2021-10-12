5c3e26ad6e7dd57fbd858c38afc8452e9c7798e6
Kirill <kirilliv.02@gmail.com>
2021-10-12 20:23:58 +0300

    12.10

--- hw3/src/main/kotlin/Main.kt
+++ hw3/src/main/kotlin/Main.kt
@@ -1,21 +1,25 @@
+import arrow.core.andThen
 import arrow.core.partially1
-
-fun main() {
-    var x: Int = calculate(Pair(sum.partially1(2), 5), Pair(minus.partially1(5), 6))
-    println(x)
-
-    x = calculate(Pair(multiply.partially1(8), 5), Pair(divide.partially1(6), 6))
-    println(x)
-}
 
 val sum = { x: Int, y: Int -> x + y }
 val minus = { x: Int, y: Int -> x - y }
 val multiply = { x: Int, y: Int -> x * y }
 val divide = { x: Int, y: Int -> x / y }
 
+fun main() {
+    println(calculate(Pair(sum.partially1(2), 5), Pair(minus.partially1(5), 6)))
+    println(calculate(Pair(multiply.partially1(8), 5), Pair(divide.partially1(6), 6)))
+}
+
 
 fun calculate(vararg params: Pair<(Int) -> Int, Int>): Int {
-    return params
-        .map { it.first(it.second) }
-        .reduce { x, y -> x + y }
+    val fnc = params
+        .map {
+            val v = it.first.partially1(it.second)
+            val f = { x: Int -> x + v() }
+            f
+        }
+        .reduce { c, n -> c andThen n }
+
+    return fnc(0)
 }
